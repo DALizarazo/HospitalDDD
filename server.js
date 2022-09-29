ed3db2bd0013cb0515939db78960505bf008cceb
diego1993alejandromail.com <Diego_Alejandro>
2022-09-28 23:03:33 -0500

    actualizado2

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const serveStatic = require('serve-static')
-const path = require('patj')
+const path = require('path')
 
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-app.get(/.*/, function (requ, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'))
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 const port = process.env.PORT || 8080
